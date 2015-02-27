3b7bc58a2dd2eb45b773240ee856dda92f7c6ab2
Ed Stephenson <edward.stephenson@me.com>
2015-02-27 16:07:27 +0000

    fixed bug

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,8 +11,6 @@
 |
 */
 
-Auth::loginUsingId(1);
-
 Route::get('/', ['as' => 'home', 'uses' => 'HomeController@index']);
 Route::get('/login', ['as' => 'login', 'uses' => 'Auth\AuthController@login']);
 
