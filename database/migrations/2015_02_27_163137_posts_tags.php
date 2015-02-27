f6d5572ee0418614605f4def3da58c398b0db246
Ed Stephenson <edward.stephenson@me.com>
2015-02-27 17:51:32 +0000

    fixed bug

--- database/migrations/2015_02_27_163137_posts_tags.php
+++ database/migrations/2015_02_27_163137_posts_tags.php
@@ -15,8 +15,8 @@ class PostsTags extends Migration {
 		Schema::create('posts_tags', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->integer('post_id');
-			$table->integer('tag_id');
+			$table->integer('posts_id');
+			$table->integer('tags_id');
 		});
 	}
 
